8efcccf7deb398cd542187a79701f9107b4062d3
my0sot1s <manhte231@gmail.com>
2017-07-14 18:21:41 +0700

    fix object to  string

--- dist/middleware.js
+++ dist/middleware.js
@@ -1,18 +1,1 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-exports.default = function (req, res, next) {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT,DELETE,OPTIONS');
-  res.set('Access-Control-Expose-Headers', 'Content-Length');
-  res.set('Access-Control-Allow-Credentials', 'true');
-  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  if (req.method == 'OPTIONS') {
-    res.status(200).end();
-  } else {
-    next();
-  }
-};
+'use strict'; Object.defineProperty(exports, "__esModule", { value: true }); exports.default = function (req, res, next) { res.set('Access-Control-Allow-Origin', '*'); res.set('Access-Control-Allow-Methods', 'GET, POST, PUT,DELETE,OPTIONS'); res.set('Access-Control-Expose-Headers', 'Content-Length'); res.set('Access-Control-Allow-Credentials', 'true'); res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type'); if (req.method == 'OPTIONS') { res.status(200).end(); } else { next(); } };
--- dist/model/ideal/nhap.js
+++ dist/model/ideal/nhap.js
@@ -1,11 +1,11 @@
 "use strict";
 
-// const tinhTong = (num) => {
-//   if (num === 1) return 1;
-//   else {
-//     return 1 / num + tinhTong(num - 1)
-//   }
-// }
+
+
+
+
+
+
 
 var tinhTong2 = function tinhTong2(num) {
   if (num === 1) return 1 / 2;else return (2 * num + 1) / (2 * num + 2) + tinhTong2(num - 1);
@@ -33,32 +33,32 @@ var tongGt = function tongGt(num) {
   if (num === 1) return 1;else return gt(num) + tongGt(num - 1);
 };
 
-// const demsl = so => {
-//   if (so == 0) return 0
-//   else {
-//     var a = so % 10;
-//     if (a % 2 !== 0)
-//       return 1 + demsl(Math.floor(so / 10))
-//     else return demsl(Math.floor(so / 10))
-//   }
-// }
 
-// const bubbleSort = () => {
-//   var mang = [3, 1, 6, 2, 9, 4, 11, 8, 8, 3]
-//   for (var i = 0; i < mang.length; i++) {
-//     for (var j = mang.length - 1; j > i; j--) {
-//       if (!mang[j - 1] || !mang[j])
-//         if (mang[j - 1] < mang[j]) {
-//           mang[j - 1] = mang[j] + mang[j - 1]
-//           mang[j] = mang[j - 1] - mang[j]
-//           mang[j - 1] = mang[j - 1] - mang[j]
-//         }
-//     }
-//   }
-//   return mang
-// }
 
-// console.log(bubbleSort())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var init = function init(mang) {
   for (var i = 0; i < mang.length; i++) {
@@ -79,10 +79,10 @@ var sinhXau = function sinhXau() {
   tryBack(1, mang, 5);
 };
 
-// Cho một mảng nn phần tử X[1, 2, …,n]X[1, 2, …,n] và một số TT.
-// Có tồn tại một tập con các phần tử của mảng XX sao cho tổng của chúng bằng TT
-// Ví dụ: X = { 8,6,7,5,3,10,9}X= { 8,6,7,5,3,10,9} và T= 12T= 12.
-// Lời giải là true vì tập con {7, 5 } { 7, 5 } của XX có tổng bằng 12.
+
+
+
+
 var subSet = function subSet(mang, r, T) {
   if (T === 0) return 1;
   if (T < 0 || r === -1) return 0;
@@ -91,7 +91,7 @@ var subSet = function subSet(mang, r, T) {
   return 0;
 };
 
-// sinhXau()
+
 
 var timXau = function timXau(mang) {
   var xau = [];
